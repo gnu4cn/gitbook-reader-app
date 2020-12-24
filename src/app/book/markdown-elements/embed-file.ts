b9e2e4a37a5e981d9f9bcfce27eb3d8bb265488c
Peng Hailin <unisko@gmail.com>
2020-12-24 17:19:15 +0800

    Rollback.

--- src/app/book/markdown-elements/embed-file.ts
+++ src/app/book/markdown-elements/embed-file.ts
@@ -20,7 +20,6 @@ const codefilesTypes = ['js', 'json'];
 @Component({
     selector: 'docspa-md-include', // tslint:disable-line
     template: ``,
-    styleUrls: ['./embed-file.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 export class EmbedMarkdownComponent implements OnInit, OnChanges {
